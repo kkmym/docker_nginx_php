07a15b8ed79a0ddfbb8a72acd3f476c8c104d100
Kentaro Kamiyama <kentarokamiyama@gmail.com>
2024-06-10 07:44:42 +0900

    主にログ関連

--- src/site/config/logging.php
+++ src/site/config/logging.php
@@ -105,6 +105,17 @@ return [
             'processors' => [PsrLogMessageProcessor::class],
         ],
 
+        'stdout' => [
+            'driver' => 'monolog',
+            'level' => env('LOG_LEVEL', 'debug'),
+            'handler' => StreamHandler::class,
+            'formatter' => env('LOG_STDERR_FORMATTER'),
+            'with' => [
+                'stream' => 'php://stdout',
+            ],
+            'processors' => [PsrLogMessageProcessor::class],
+        ],
+
         'syslog' => [
             'driver' => 'syslog',
             'level' => env('LOG_LEVEL', 'debug'),
